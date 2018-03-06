ea9176f6dcd0bde950baee18d2fad879376923cd
Farago Bence <semmiertelme13@gmail.com>
2018-03-06 16:26:57 +0100

    DataGenerator returns 1 Measurement

--- src/main/java/com/codecool/greencommitment/client/DataGenerator.java
+++ src/main/java/com/codecool/greencommitment/client/DataGenerator.java
@@ -10,59 +10,36 @@ import java.util.List;
 import java.util.Random;
 
 public class DataGenerator {
-    private int numberOfMeasurement;
-    private int min;
-    private int max;
-    private String type;
-
+    private Type type;
 
 
     private Random random = new Random();
 
-    DataGenerator(int numberOfMeasurement, int min, int max, String type) {
-        this.numberOfMeasurement = numberOfMeasurement;
-        this.min = min;
-        this.max = max;
+    DataGenerator(Type type) {
         this.type = type;
     }
 
 
-    public List<Measurement> createData() {
+    public Measurement createData() {
         Measurement measure = null;
         List<Measurement> list = new ArrayList<>();
         long time = Instant.now().toEpochMilli();
-        for (int i = 0; i < numberOfMeasurement ; i++) {
-            time += 2000;
-            int unit = random.nextInt(max-min) + min;
-            String unitOfMeasurement;
-            if (type.equals("temp")) {
+        time += 2000;
+        String unitOfMeasurement;
+        int unit;
+        switch (type) {
+            case Type.TEMPERATURE:
+                unit = 15 + random.nextInt(30);
                 unitOfMeasurement = "Celsius";
-                measure = new TemperatureMeasurement(time, unit,unitOfMeasurement );
-            }
-            else if (type.equals("moisture")) {
+                measure = new TemperatureMeasurement(time, unit, unitOfMeasurement);
+                return measure;
+            case Type.MOISTURE:
+                unit = 45 + random.nextInt(70);
                 unitOfMeasurement = "%";
                 measure = new MoistureMeasurement(time, unit, unitOfMeasurement);
-            }
-
-           list.add(measure);
+                return measure;
         }
-        return list;
+        return measure;
     }
 
-    public Measurement getRandomMeasurement() {
-        Measurement result = null;
-        String unitOfMeasurement;
-        int unit = random.nextInt(max-min) + min;
-        long time = Instant.now().toEpochMilli();
-
-        if (type.equals("temp")) {
-            unitOfMeasurement = "Celsius";
-            result = new TemperatureMeasurement(time, unit,unitOfMeasurement );
-        }
-        else if (type.equals("moisture")) {
-            unitOfMeasurement = "%";
-            result = new MoistureMeasurement(time, unit, unitOfMeasurement);
-        }
-        return result;
-    }
 }
